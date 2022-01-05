255f571aff54f584d8eabbc4629b26c8e5816f58
valeriocipolla <valeriocipolla92@gmail.com>
2022-01-05 15:20:27 +0000

    task 1 completed

---
+++ src/components/Input/index.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function Input() {
+  return <input type="text" />;
+}
+
+export default Input;
---
+++ src/components/Item/index.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function Item() {
+  return <li></li>;
+}
+
+export default Item;
